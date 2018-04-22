d709bf1b111baa28d2871afeb48913e79895a814
Theresa <theresa.l.morelli@gmail.com>
2018-04-22 22:03:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const rank = document.getElementById('app').querySelectorAll('.ranked-list li');
+
+  for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (parseInt(rank[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  while (node.hasChildNodes()) {
+    node = node.firstElementChild;
+    if (!node.firstElementChild) {
+      return node;
+    }
+  }
+}
